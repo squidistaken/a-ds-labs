adcd071f7c50f59a524691ee728924b891043fa5
Marcus Persson <marcusper2003@gmail.com>
2024-02-13 16:00:08 +0100

    update: recognize_equation

--- Lab 2/recognizeEq.py
+++ Lab 2/recognizeEq.py
@@ -1,13 +1,16 @@
 """
-File:   recognizeEq.py
+File: recognizeEq.py
 Author: Harmen de Weerd (harmen.de.weerd@rug.nl)
+Contributors:
+    Marcus Persson (m.h.o.persson@student.rug.nl),
+    Marinus van den Ende (m.van.den.ende.1@student.rug.nl)
 
 Description:
-    A recognizer that determines whether a given token list
-    represents a valid equation
+    A recognizer that determines whether a given token list represents a valid equation.
+    Modified by Marcus Persson (m.h.o.persson@student.rug.nl),
+    Marinus van den Ende (m.van.den.ende.1@student.rug.nl).
 """
 from scanner import *
-
 
 def recognize_equation(tokenlist: TokenList) -> bool:
     """
@@ -15,6 +18,14 @@ def recognize_equation(tokenlist: TokenList) -> bool:
     :param tokenlist: input token list
     :return: True iff the token list represents a valid equation
     """
+    equation = tokenlist.__str__()
+
+    if "(" in equation or ")" in equation:
+        return False
+    if equation.find("=") == 0 or equation.find("=") == len(equation) - 1:
+        return False
+    if equation.count("=") == 1:
+        return True
     return False
 
 
@@ -29,12 +40,8 @@ def get_degree(tokenlist: TokenList) -> int:
 
 def is_single_variable_equation(tokenlist: TokenList) -> bool:
     """
-    Determines whether a token list contains exactly one identifir
+    Determines whether a token list contains exactly one identifier
     :param tokenlist: input token list
     :return: True iff the token list contains exactly one identifier
     """
     return False
-
-
-
-
