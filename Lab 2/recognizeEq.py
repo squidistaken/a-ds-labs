7a92cb70c71d44bab77a62962568adb6f43b627d
Marcus Persson <marcusper2003@gmail.com>
2024-02-15 23:56:19 +0100

    reattempt: using tokenlistpositions instead
    probably smarter to actually follow the reader lol

--- Lab 2/recognizeEq.py
+++ Lab 2/recognizeEq.py
@@ -12,21 +12,111 @@ Description:
 """
 from scanner import *
 
+
+class TokenListPosition:
+    def __init__(self, tokens: TokenList):
+        self.tokens = tokens
+
+
+def _accept_number(position: TokenListPosition) -> bool:
+    """
+    Reads a number off the token list, if it exists
+    :param position: pointer to a token list node
+    :return: True iff a number has been read off the token list
+    """
+    if position.tokens is not None and position.tokens._type == TokenType.NUMBER:
+        position.tokens = position.tokens._next
+        return True
+    return False
+
+
+def _accept_identifier(position: TokenListPosition) -> bool:
+    """
+    Reads an identifier off the token list, if it exists
+    :param position: pointer to a token list node
+    :return: True iff an identifier has been read off the token list
+    """
+    if position.tokens is not None and position.tokens._type == TokenType.IDENTIFIER:
+        position.tokens = position.tokens._next
+        return True
+    return False
+
+
+def _accept_symbol(position: TokenListPosition, symbol: str) -> bool:
+    """
+    Reads a symbol off the token list, if it exists
+    :param position: pointer to a token list node
+    :return: True iff a symbol has been read off the token list
+    """
+    if position.tokens is not None and position.tokens._type == TokenType.SYMBOL and position.tokens._value == symbol:
+        position.tokens = position.tokens._next
+        return True
+    return False
+
+
+def _accept_exponent(position: TokenListPosition) -> bool:
+    """
+    Reads an exponent off the token list, if it exists
+    :param position: pointer to a token list node
+    :return: True iff an exponent has been read off the token list
+    """
+    return _accept_number(position)
+
+
+def _accept_term(position: TokenListPosition) -> bool:
+    """
+    Reads a maximal term off the token list, if it exists
+    :param position: pointer to a token list node
+    :return: True iff a term has been read off the token list
+    """
+    # <term> ::= <nat> | [<nat>] <identifier> ['^' <nat>]
+
+    # [<nat>] <identifier> ['^' <nat>]
+    if not _accept_number(position):
+        if not _accept_identifier(position):
+            if not _accept_symbol(position, "^") and not _accept_number(position):
+                return False
+
+    while _accept_symbol(position, "^"):
+        if not _accept_exponent(position):
+            return False
+    return True
+
+
+def _accept_expression(position: TokenListPosition) -> bool:
+    """
+    Reads a maximal expression off the token list, if it exists
+    :param position: pointer to a token list node
+    :return: True iff an expression has been read off the token list
+    """
+    # <expression> :== ['-'] <term> {'+' <term> | '-' <term>}
+
+    # ['-'] <term>
+    if not _accept_symbol(position, "-"):
+        if not _accept_term(position):
+            return False
+    while _accept_symbol(position, "+") or _accept_symbol(position, "-"):
+        if not _accept_term(position):
+            return False
+    return True
+
+
+def _accept_equation(position: TokenListPosition) -> bool:
+    # <equation> ::= <expression> '=' <expression>
+    while _accept_expression(position) or _accept_symbol(position, "="):
+        if not _accept_expression(position):
+            return False
+    return True
+
+
 def recognize_equation(tokenlist: TokenList) -> bool:
     """
     Determines whether the given token list represents a valid equation
     :param tokenlist: input token list
     :return: True iff the token list represents a valid equation
     """
-    equation = tokenlist.__str__()
-
-    if "(" in equation or ")" in equation:
-        return False
-    if equation.find("=") == 0 or equation.find("=") == len(equation) - 1:
-        return False
-    if equation.count("=") == 1:
-        return True
-    return False
+    position = TokenListPosition(tokenlist)
+    return _accept_equation(position) and position.tokens is None
 
 
 def get_degree(tokenlist: TokenList) -> int:
