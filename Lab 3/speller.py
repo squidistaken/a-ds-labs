26c431a62d117034807de24b80e57a198290f30f
Marcus Persson <marcusper2003@gmail.com>
2024-02-27 11:03:36 +0100

    finish part 1
    very easy lmao

--- Lab 3/speller.py
+++ Lab 3/speller.py
@@ -1,8 +1,11 @@
 """
-File:   speller.py
+File: speller.py
+Contributors:
+    Marcus Persson (m.h.o.persson@student.rug.nl),
+    Marinus van den Ende (m.van.den.ende.1@student.rug.nl)
 
 Description:
-    A spell checker
+    A spell checker. Modified by Marcus Persson and Marinus van den Ende.
 """
 
 
@@ -13,13 +16,18 @@ while word != "!":
     allowed_words.append(word)
     word = input()
 
+splitters = [".", ",", "\'", "’", "/", "-", "\"", ":", ";", "?", "!", "(", ")"]
+
 # Step 2: check words
 word = input()
 unknown_word_count = 0
 while word != "":
-    if word not in allowed_words:
-        print(word)
-        unknown_word_count += 1
+    for splitter in splitters:
+        word = " ".join(word.lower().split(splitter))
+    for w in word.split():
+        if w not in allowed_words:
+            print(w)
+            unknown_word_count += 1
     word = input()
 
 # TODO: Replace the above while loop with a correct solution.
