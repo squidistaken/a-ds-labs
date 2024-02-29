d2a34a9eab82ecc5fd4bd212ccdc3963f79ca7e9
Marcus Persson <marcusper2003@gmail.com>
2024-02-29 12:21:47 +0100

    im gonna merge now

--- Lab 3/speller_trie.py
+++ Lab 3/speller_trie.py
@@ -19,8 +19,10 @@ while word != "!":
 
 # Step 2: Validate if words exist in a given input.
 # Handle non-alphabetical characters.
-splitters = [".", ",", "\'", "’", "/", "-", "\"", ":", ";", "?", "!", "(", ")",
-             "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+splitters = [".", ",", "\'", "’", "/", "-", "\"", ":", ";", "?",
+             "!", "(", ")", "1", "2", "3", "4", "5", "6", "7", "8",
+             "9", "0"]
+
 unknown_word_count = 0
 
 word = input()
