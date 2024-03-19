6b56cf56e5a6c4d43bb265b530e0f086b871f750
Marcus Persson <marcusper2003@gmail.com>
2024-03-19 11:46:54 +0100

    setup: inputs
    Done through tuples and lists.

--- Lab 5/lab5trains.py
+++ Lab 5/lab5trains.py
@@ -7,5 +7,21 @@ Description:
     that route, including the starting and ending station, as well as the total time the connection will take.
 """
 
-# Insert code moment
+# Input: n current disruptions.
+disruptions = int(input())
 
+# Input: n many disruptions, in which each disruption consists of a tuple
+#        regarding a direct connection.
+banned_connections = []
+for i in range(disruptions):
+    connection = (input(), input())
+    banned_connections.append(connection)
+
+# Input: Queries, in which each query consists of a tuple regarding a
+#        start and end. Program ends when input == "!".
+station = input()
+while station != "!":
+    query = (station, input())
+    # Do something here
+
+    station = input()
